a18dadd00420b3aeecf7ffa4cc303336aa308ca3
Emily Bland <emilyrbland1995@gmail.com>
2020-03-27 11:53:09 +0000

    Change deck in generateDeckColours.js to Deck

--- deckPicker/js/generateDeckColours.js
+++ deckPicker/js/generateDeckColours.js
@@ -2,35 +2,35 @@ function generateDeckColours() {
  let randomNumber = Math.floor(Math.random() * 15);
   switch (randomNumber) {
     case 0:
-      return 'Build a Black deck';
+      return 'Build a Black Deck';
     case 1:
-      return 'Build a Red deck';
+      return 'Build a Red Deck';
     case 2:
-      return 'Build a Green deck';
+      return 'Build a Green Deck';
     case 3:
-      return 'Build a Blue deck';
+      return 'Build a Blue Deck';
     case 4:
-      return 'Build a Black & Red deck';
+      return 'Build a Black & Red Deck';
     case 5:
-      return 'Build a Black & Green deck';
+      return 'Build a Black & Green Deck';
     case 6:
-      return 'Build a Black & Blue deck';
+      return 'Build a Black & Blue Deck';
     case 7:
-      return 'Build a Red & Green deck';
+      return 'Build a Red & Green Deck';
     case 8:
-      return 'Build a Red & Blue deck';
+      return 'Build a Red & Blue Deck';
     case 9:
-      return 'Build a Green & Blue deck';
+      return 'Build a Green & Blue Deck';
     case 10:
-      return 'Build a White deck';
+      return 'Build a White Deck';
     case 11:
-      return 'Build a White & Black deck';
+      return 'Build a White & Black Deck';
     case 12:
-      return 'Build a White & Red deck';
+      return 'Build a White & Red Deck';
     case 13:
-      return 'Build a White & Green deck';
+      return 'Build a White & Green Deck';
     case 14:
-      return 'Build a White & Blue deck';
+      return 'Build a White & Blue Deck';
     default:
       return 'Please try again.';
   }
